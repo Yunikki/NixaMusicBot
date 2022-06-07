c88c6d9e27b7f315d89f3023ddef154b5f858e08
Zᴇʙʀᴀ <96722810+Zebra012@users.noreply.github.com>
2022-06-08 01:46:58 +0530

    Update utils.py

--- NIXA/utils.py
+++ NIXA/utils.py
@@ -4,11 +4,11 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"Zaid/plugins/{plugin_name}.py")
-    name = "Zaid.plugins.{}".format(plugin_name)
+    path = Path(f"NIXA/plugins/{plugin_name}.py")
+    name = "NIXA.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["Zaid.plugins." + plugin_name] = load
-    print("Bot has Started " + plugin_name)
+    sys.modules["NIXA.plugins." + plugin_name] = load
+    print("••• ʙᴏᴛ ʜᴀs sᴛᴀʀᴛᴇᴅ ••• " + plugin_name)
