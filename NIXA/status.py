a11fba48204810c3323870a03f1e1da1e1ca2233
Zᴇʙʀᴀ <96722810+Zebra012@users.noreply.github.com>
2022-06-08 01:48:06 +0530

    Update status.py

--- NIXA/status.py
+++ NIXA/status.py
@@ -14,5 +14,5 @@ def is_admin(func):
         if is_admin:
             await func(event, _s)
         else:
-            await event.reply("Only Admins can execute this command!")
+            await event.reply("» ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴇxᴇᴄᴜᴛᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
     return a_c
