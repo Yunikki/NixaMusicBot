f9003b4837c70fd2f0726785c72fff4fd3003523
Zᴇʙʀᴀ <96722810+Zebra012@users.noreply.github.com>
2022-06-08 02:19:41 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,3 +5,4 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
