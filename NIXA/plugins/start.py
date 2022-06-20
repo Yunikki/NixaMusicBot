3d598cb347e39c74a37e50ae28754e74e448c53a
Yash <93755124+Huvaris@users.noreply.github.com>
2022-06-20 14:50:40 +0530

    Update start.py

--- NIXA/plugins/start.py
+++ NIXA/plugins/start.py
@@ -6,17 +6,17 @@ from Config import BOT_USERNAME
 async def start(event):
     await event.reply("ʜᴇʟʟᴏ ɪ ᴀᴍ sᴜᴘᴇʀ ғᴀsᴛ ᴍᴜsɪᴄ ʙᴏᴛ
 ᴀɴᴅ ɴᴏ ʟᴀɢ ɪssᴜᴇ ᴡɪᴛʜ ʙᴇsᴛ sᴏᴜɴᴅ ǫᴜᴀʟɪᴛʏ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs. ᴛʜɪs ᴍᴜsɪᴄ ʙᴏᴛ ʙᴀsᴇᴅ ᴏɴ ᴛᴇʟᴇᴛʜᴏɴ
- ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ sᴜᴍɪᴛ ʏᴀᴅᴀᴠ...",
+ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ʏᴀꜱʜ ...",
                     buttons=[
                         [
                           Button.url("➕ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕", url="https://t.me/{BOT_USERNAME}?startgroup=true")],
                        ],
                        [
-                          Button.url("• sᴜᴘᴘᴏʀᴛ •", url="https://t.me/TechQuardSupport"),
+                          Button.url("• sᴜᴘᴘᴏʀᴛ •", url="https://t.me/FRIENDS_DRAMA_CLUB"),
                           Button.url("• ᴜᴘᴅᴀᴛᴇs •", url="https://t.me/TechQuard")
                        ],
                        [
-                          Button.url("• ᴅᴇᴠᴇʟᴏᴘᴇʀ •", url="https://t.me/Simple_Mundaa"),
+                          Button.url("• ᴅᴇᴠᴇʟᴏᴘᴇʀ •", url="https://t.me/give_up_to_god"),
                           Button.url("• ʏᴏᴜᴛᴜʙᴇ •", url="https://youtube.com/channel/UCtI7hbY-BD7wvuIzoSU0cEw")
                        ]
                        )
